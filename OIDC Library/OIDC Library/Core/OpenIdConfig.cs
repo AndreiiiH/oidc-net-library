2d95737f78ed95918a08a438e41686fdc5f57036
AndreiiiH <hava.ionut@gmail.com>
2017-12-20 15:35:08 +0000

    Version 1.2.2-alpha: Fixes a small async bug

--- OIDC Library/OIDC Library/Core/OpenIdConfig.cs
+++ OIDC Library/OIDC Library/Core/OpenIdConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.IdentityModel.Protocols;
 
 namespace ChaoticPixel.OIDC.Core
@@ -6,27 +7,23 @@ namespace ChaoticPixel.OIDC.Core
     public class OpenIdConfig
     {
         // public ConfigurationManager<OpenIdConnectConfiguration> ConfigManager { get; }
-        public OpenIdConnectConfiguration Config { get; }
+        public OpenIdConnectConfiguration Config { get; set; }
         public string Nonce { get; }
         public string ClientId { get; }
         public string ClientSecret { get; }
 
-        private OpenIdConfig(string clientId, string clientSecret, OpenIdConnectConfiguration config)
+        public OpenIdConfig(string clientId, string clientSecret)
         {
-            Config = config;
-
             ClientId = clientId;
             ClientSecret = clientSecret;
 
             Nonce = Core.Nonce.Generate();
         }
 
-        public static async Task<OpenIdConfig> CreateConfig(string openIdConfigUrl, string clientId, string clientSecret)
+        public async Task GetConfig(string openIdConfigUrl)
         {
             ConfigurationManager<OpenIdConnectConfiguration> configManager = new ConfigurationManager<OpenIdConnectConfiguration>(openIdConfigUrl);
-            OpenIdConnectConfiguration config = await configManager.GetConfigurationAsync();
-
-            return new OpenIdConfig(clientId, clientSecret, config);
+            Config = await configManager.GetConfigurationAsync();
         }
     }
 }
